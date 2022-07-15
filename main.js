8f027fd9f2d1dc54a805c582b460d044f5a08586
Abirup112 <82591730+Abirup112@users.noreply.github.com>
2022-07-15 19:24:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ song = "";
 
 function preload()
 {
-	song = loadSound("Ben 10 Alien Force.mp3");
+	song = loadSound("Believer.mp3");
 }
 
-
+rightWristY=0;
+rightWristX=0;
 
 leftWristX = 0;
 leftWristY = 0;
-
+scoreLeftWrist=0;
+scoreRightWrist=0;
 function setup() {
 	canvas =  createCanvas(600, 500);
 	canvas.center();
@@ -29,7 +31,8 @@ function gotPoses(results)
 {
   if(results.length > 0)
   {
-	
+	console.log(results);
+	scoreRightWrist=results[0].pose.keypoints[10].score;
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
 	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
 	
@@ -49,17 +52,38 @@ function draw() {
 
 	fill("#FF0000");
 	stroke("#FF0000");
-
+circle(rightWristX, rightWristY, 20);
+if(scoreRightWrist>0.2){
+	if(rightWristY>0 && rightWristY<=100){
+		document.getElementById("speed").innerHTML="Speed = 0.5x";
+		song.rate(0.5);
+	}
+	else if(rightWristY>100 && rightWristY <=200){
+		document.getElementById("speed").innerHTML="Speed = 1x";
+		song.rate(1);
+	}
+    else if(rightWristY > 200 && rightWristY <= 300){
+		document.getElementById("speed").innerHTML="Speed = 1.5x";
+		song.rate(1.5);
+	}
+	else if(rightWristY > 300 && rightWristY <= 400){
+		document.getElementById("speed").innerHTML="Speed = 2.0x";
+		song.rate(2);
+	}
+	else if(rightWristY > 400 && rightWristY <= 500){
+		document.getElementById("speed").innerHTML="Speed = 2.5x";
+		song.rate(2.5);
+	}
+}
 	
-
 	if(scoreLeftWrist > 0.2)
 	{
 		circle(leftWristX,leftWristY,20);
 		InNumberleftWristY = Number(leftWristY);
-		new_leftWristY = floor(InNumberleftWristY *2);
-		leftWristY_divide_1000 = new_leftWristY/1000;
-		document.getElementById("volume").innerHTML = "Volume = " + leftWristY_divide_1000;		
-		song.setVolume(leftWristY_divide_1000);	
+		remove_decimals = floor(InNumberleftWristY);
+		volume=remove_decimals/500;
+		document.getElementById("volume").innerHTML = "Volume = " + remove_decimals;		
+		song.setVolume(remove_decimals);	
 	}
 
 }
